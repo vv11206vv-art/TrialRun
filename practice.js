19cd3f770ec81c3ea583bb456af4475c46f252a4
Nahom Codes <nahmer2004@gmail.com>
2026-05-01 12:43:38 +0200

    From this, i have learned the falling: state machine, conversation flow, intent detection, fallback logic

---
+++ practice.js
@@ -0,0 +1,50 @@
+let now = {
+    state : "start"
+};
+
+function greeting(){
+            console.log("LETS START. I am your DOCTOR APPOINTMENT BOT. Just tell me whether you are ill")
+}
+function bot(message){
+    message = message.toLowerCase()
+    if (now.state === "start"){
+        if (message.includes("ill")){
+            now.state = "symptoms"
+            return("What sympotms do you have?")
+        } else {
+            return "Sorry please tell me wheather you are ill"
+        }
+    }
+    if (now.state === "symptoms"){
+        if (true){
+            now.state = "choose"
+            return("Ohhh, sorry to hear that. Do you want a GP or S?")
+        }
+    }
+    if(now.state === "choose"){
+        if (message.includes("gp")){
+            now.state = "confirm"
+            return("Confirm appointment with GP?")
+        } else if (message.includes("s")){
+            now.state = "confirm"
+            return("Confirm appointment with S?")
+        }else {
+            return("Sorry choose S or GP")
+        }
+    }
+    if(now.state === "confirm"){
+        if (message.includes("confirm") || message.includes("Yes")){
+            now.state = "start"
+            return "Appointment booked!"
+
+        } else if (message.includes("no") || message.includes("Not")){
+            now.state = "start"
+
+            return("Okay, booking canceled")
+        } else{
+            return("Please tell me to confirm or not")
+        }
+    }
+}
+
+greeting()
